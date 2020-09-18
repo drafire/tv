5e75ed037bdf030b3d35e25258e799734374d043
drafire <hyljljlj@163.com>
2020-09-18 20:09:30 +0800

    first commit

---
+++ entry/src/main/java/com/drafire/tv/TV.java
@@ -0,0 +1,10 @@
+package com.drafire.tv;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class TV extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/com/drafire/tv/MainAbilityTest.java
@@ -0,0 +1,9 @@
+package com.drafire.tv;
+
+import org.junit.Test;
+
+public class MainAbilityTest {
+    @Test
+    public void onStart() {
+    }
+}
